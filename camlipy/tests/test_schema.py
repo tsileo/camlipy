7fa3afab519525ec4ef40256fd1e901b99c0507f
Thomas Sileo <thomas.sileo@gmail.com>
2013-07-31 21:38:36 +0200

    Oops, removed empty file.

--- camlipy/tests/test_schema.py
+++ camlipy/tests/test_schema.py
@@ -1,59 +1,15 @@
 # -*- encoding: utf-8 -*-
 import unittest
-import os
 import logging
-import tempfile
+
+from camlipy.tests import CamliPyTestCase
 
 logging.basicConfig(level=logging.DEBUG)
 
-import camlipy
-from camlipy import Camlistore, compute_hash
 
-camlipy.DEBUG = True
-CAMLIPY_SERVER = os.environ.get('CAMLIPY_SERVER', 'http://localhost:3179/')
-
-
-class CamliPyTestCase(unittest.TestCase):
-    def setUp(self):
-        self.server = Camlistore(CAMLIPY_SERVER)
-
-    def testPutBlobStr(self):
-        test_blob_str = os.urandom(4096)
-        resp = self.server.put_blobs([test_blob_str])
-        self.assertEqual(resp['received'], [{'blobRef': compute_hash(test_blob_str), 'size': 4096}])
-
-    def testPutBlobFileobj(self):
-        test_blob_file = tempfile.TemporaryFile()
-        test_blob_file.write(os.urandom(4096))
-        test_blob_file.seek(0)
-
-        resp = self.server.put_blobs([test_blob_file])
-        test_blob_file.seek(0)
-        self.assertEqual(resp['received'], [{'blobRef': compute_hash(test_blob_file.read()), 'size': 4096}])
-
-    def testGetBlob(self):
-        data_len = (1024 << 10) + (4 << 10)
-        blob_data = os.urandom(data_len)
-        blob_br = compute_hash(blob_data)
-        self.server.put_blobs([blob_data])
-
-        fileobj = self.server.get_blob(blob_br)
-        self.assertTrue(isinstance(fileobj, tempfile.SpooledTemporaryFile))
-        self.assertEqual(len(fileobj.read()), data_len)
-        fileobj.seek(0)
-        self.assertEqual(fileobj.read(), blob_data)
-
-    def testGetBlobSchema(self):
+class CamliPyTestCase(CamliPyTestCase):
+    def testBasicSchema(self):
         pass
-
-    def testStat(self):
-        test_blob_str = os.urandom(4096)
-        blob_br = compute_hash(test_blob_str)
-        resp = self.server.put_blobs([test_blob_str])
-        self.assertEqual(blob_br, resp['received'][0]['blobRef'])
-
-        stat_resp = self.server._stat([blob_br])
-        self.assertEqual(stat_resp['stat'][0], {'blobRef': blob_br, 'size': 4096})
 
 if __name__ == '__main__':
     unittest.main()
