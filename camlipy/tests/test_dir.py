bea91663d60c2983990bb9678b9dc33bf1c4f50c
Thomas Sileo <thomas.sileo@gmail.com>
2013-08-19 23:44:54 +0200

    Better directory tests.

--- camlipy/tests/test_dir.py
+++ camlipy/tests/test_dir.py
@@ -2,6 +2,7 @@
 import os
 import logging
 import tempfile
+import shutil
 
 from dirtools import Dir
 
@@ -14,12 +15,23 @@ log = logging.getLogger(__name__)
 
 class testDir(CamliPyTestCase):
     def testDirectory(self):
-        dir_br = put_directory(self.server, '/work/writing')
+        tmpdir = tempfile.mkdtemp()
+        with open(os.path.join(tmpdir, 'testfile1'), 'wb') as fh:
+            fh.write(os.urandom(256 << 10))
+        testdir = os.path.join(tmpdir, 'testdir')
+        os.mkdir(testdir)
+        with open(os.path.join(testdir, 'testfile2'), 'wb') as fh:
+            fh.write(os.urandom(256 << 10))
+
+        dir_br = put_directory(self.server, tmpdir)
 
         dest = tempfile.mkdtemp()
 
         get_directory(self.server, dir_br, dest)
 
         # Check the two directories are equal using Dirtools.hash
-        self.assertEqual(Dir('/work/writing').hash(),
-                         Dir(os.path.join(dest, 'writing')).hash())
+        self.assertEqual(Dir(tmpdir).hash(),
+                         Dir(dest).hash())
+
+        shutil.rmtree(tmpdir)
+        shutil.rmtree(dest)
